d6a59b2bf030828ca9a4647ac8a5728e95b711ac
slavamishch1979@gmail.com <slavamishch1979@gmail.com>
2022-06-04 09:34:21 +0300

    homeworkLesson3

---
+++ myBubbles/myBubbles/myBubbles.cpp
@@ -0,0 +1,52 @@
+﻿// myBubbles.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+// My version Bubbles
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter size of array:  ";
+    int sizeArray;
+    std::cin >> sizeArray;
+    int* myArray = new int[sizeArray];
+    std::cout << "Enter elements of array:" << std::endl;
+    int i = 0;
+    while (i != sizeArray)
+    {
+        std::cin >> myArray[i];
+        i++;
+    }
+    std::cout << "You've got array : {";
+    for (int k = 0; k < sizeArray; k++)
+    {
+        std::cout << myArray[k] << " ";
+    }
+    std::cout << "}" << std::endl;
+
+    for (int k = 0; k < sizeArray; k++)
+    {
+        for (int p = k + 1; p < sizeArray; p++)
+        {
+
+            if (myArray[k] > myArray[p])
+            {
+                int tempArray{};
+                tempArray = myArray[k];
+                myArray[k] = myArray[p];
+                myArray[p] = tempArray;
+            }
+        }
+    }
+    
+    std::cout << "You've got sorted array : {";
+    for (int k = 0; k < sizeArray; k++)
+    {
+        std::cout << myArray[k] << " ";
+    }
+    std::cout << "}";
+
+    return 0;
+}
