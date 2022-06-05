dff417956a74087435fc479cf655d913e2b0ac26
vngbro <vladimir.golubenko@gmail.com>
2022-06-05 20:40:12 +0300

    homework_3_arr

---
+++ les2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    const int size = 10;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Need to enter ten elements in array.\n\n";
+        cout << " Please enter \"" << i + 1 << "\" data element = ";
+        cin >> *(arr+i);
+        system("cls");
+    }
+   
+    for (int j = 0; j < size; j++)
+    {
+        for (int i = 1; i < size; i++)
+        {
+            if (*(arr + i) < *(arr + (i-1))) swap(*(arr + i), *(arr + (i - 1)));
+        }
+    }
+
+    cout << "Sorted array of data:\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << " arr[" << i<< "] = " << *(arr + i) << endl;
+    }
+    
+    return 0;
+}
