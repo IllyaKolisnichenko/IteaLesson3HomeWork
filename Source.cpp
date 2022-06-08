caa7cfb2d96646457cd46b0d077524c30a8bb81f
ZaloginYuriy <yuriyzalogin31031981@gmail.com>
2022-06-08 12:08:22 +0300

    KUB1

--- Source.cpp
+++ Source.cpp
@@ -5,33 +5,32 @@
 using namespace std;
 void main()
 {
-	//setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");
 	srand(time(0));
 	int const size = 10;
 	int array[size];
 	cout << "intput array :" << endl;
-	
-	
+
+
 	for (int i = 0;i < size;i++)
-		array = array[i];
-			cin >> *(array+1);
+		cin >> *(array + i);
 
-		
-		for (int i = 0;i < size;i++)
-		{
-			for (int j = size - 1;j > i;j--)
-			{
-				if (*(array[j]) <*( array[j - 1]))
-					swap(*(array[j]), *(array[j - 1]));
 
-			}
-		}
-		for (int i = 0;i < size;i++)
+	for (int i = 0;i < size;i++)
+	{
+		for (int j = size - 1;j > i;j--)
 		{
-			
-			cout << "To access the first element, go to this pointer " << *(array + 1) << endl;
+			if (*(array + j) < *(array + (j - 1)))
+				swap(*(array + j), *(array + (j - 1)));
 
 		}
-		cout << endl;
+	}
+	for (int i = 0;i < size;i++)
+	{
+
+		cout << "To access the first element, go to this pointer " << *(array + i) << endl;
+
+	}
+	cout << endl;
 
 }
