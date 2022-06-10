4bdb670de757ebdd7caeb780b5979483cb212aba
Ivan Bachynskyi <i.bachinsky77@gmail.com>
2022-06-10 16:01:19 +0300

    lesson3 output added

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ int main() {
     const int size = 10;
     int numbersArray[size]{};
 
+    std::cout << "Enter 10 numbers:\n";
     for(int i = 0; i < size; i++){
         std::cin >> numbersArray[i];
     }
     bubbleSort(numbersArray, size);
+    std::cout << "Your numbers have been sorted: ";
     for(int j = 0; j < size; j++){
         std::cout << numbersArray[j];
     }
