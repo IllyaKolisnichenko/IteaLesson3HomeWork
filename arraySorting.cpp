8b2afa0ac109b9d8d2f68fb6af0a20e35bf8a490
unknown <nimas.andriy@gmail.comgit config --global user.email nimas.andriy@gmail.comgit config --global user.email nimas.andriy@gmail.com>
2022-06-13 15:40:45 +0300

    homework_2_2

--- arraySorting.cpp
+++ arraySorting.cpp
@@ -5,22 +5,22 @@ int main()
 {
     const int arrSize{10};
     int arrNumber[arrSize];
-    
+
     // itinialization array
     for (int i{}; i < arrSize; i++)
     {
         std::cout << "Enter " << i + 1 << " number of array: ";
         std::cin >> *(arrNumber + i);
     }
-    
+
     // array sorting
     for (int i{}; i < arrSize; i++)
     {
-        for (int j{i+1}; j < arrSize; j++)
+        for (int j{i + 1}; j < arrSize; j++)
         {
             if (*(arrNumber + j) < *(arrNumber + i))
             {
-                std::swap (*(arrNumber + i), *(arrNumber + j));
+                std::swap(*(arrNumber + i), *(arrNumber + j));
             }
         }
     }
@@ -28,8 +28,8 @@ int main()
     // printing sorted array
     std::cout << "Sorted array: ";
     for (int i{}; i < arrSize; i++)
-        {
-            std::cout << *(arrNumber + i) << " ";
-        }  
-return 0;
+    {
+        std::cout << *(arrNumber + i) << " ";
+    }
+    return 0;
 }
