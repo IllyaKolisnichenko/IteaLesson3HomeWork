58e53365e2b5754fee4c756a36402a8a6a5219b0
vania <89963197+vaniagbe@users.noreply.github.com>
2022-06-26 11:19:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,21 @@
-#include <iostream> 
-#include <algorithm> 
-using namespace std;
-//Масив.Сортування вибором.
-
+#include <iostream>
+#include<algorithm>
 
 int main()
-{ 
-	setlocale(LC_ALL, "ukr");
-	const int length = 10;
-	int arr[length] = { };
-	for (int i = 0; i < length; i++)
-	{
-		cout << "Заповни масив : " ;
-		cin >> arr[i];
-	}
+{
+    const int Size = 10;
+    int array[Size] = {};
+    for (int i = 0; i < Size; i++)
+    {
+        std::cin >> array[i];
+    }
+    std::sort(array, array + Size);
+    int* pArr = array;
+    for (int i = 0; i < Size; i++)
+    {
+        std::cout << pArr[i] << std::endl;
+    }
+    
 
-	for (int startIndex = 0; startIndex < length - 1; ++startIndex)
-	{
-
-		int smallestIndex = startIndex;
-
-
-		for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
-		{
-
-			if (arr[currentIndex] < arr[smallestIndex])
-
-				smallestIndex = currentIndex;
-		}
-
-
-		swap(arr[startIndex], arr[smallestIndex]);
-	}
-
-	cout << "\n__________Процес сортування__________\n"<<endl;
-	for (int index = 0; index < length; ++index)
-		cout << arr[index] << ' ';
-
-	return 0;
-
+    return 0;
 }
